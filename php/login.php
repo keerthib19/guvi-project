f4336a4602cdb9a43eabb2169e52c4f2f3d43b63
keerthib19 <130020480+keerthib19@users.noreply.github.com>
2023-04-09 17:34:55 +0530

    Update login.php

--- php/login.php
+++ php/login.php
@@ -20,6 +20,50 @@ function db_query(string $query, array $data = array())
 
 	return false;
 }
+function is_logged_in():bool
+{
+
+	if(!empty($_SESSION['PROFILE']))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+function redirect($path):void
+{
+	header("Location: $path");
+	die;
+}
+
+function esc(string $str):string
+{
+	return htmlspecialchars($str);
+}
+
+function get_image($path = ''):string 
+{
+	if(file_exists($path))
+	{
+		return $path;
+	}
+
+	return './images/no-image.jpg';
+}
+
+function user(string $key = '')
+{
+	if(is_logged_in())
+	{
+		if(!empty($_SESSION['PROFILE'][$key]))
+		{
+			return $_SESSION['PROFILE'][$key];
+		}
+	}
+
+	return false;
+}
 if(!empty($_POST['data_type']))
 {
 	$info['data_type'] 	= $_POST['data_type'];
